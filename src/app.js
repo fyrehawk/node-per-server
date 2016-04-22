b25c5244e8a12cd3e8b3de91e26a8eb4e5e62020
Fyre Hawk <fyrehawk@tutanota.com>
2016-04-22 14:38:26 +0000

    Initial commit.

---
+++ src/app.js
@@ -0,0 +1,26 @@
+var WebSocketServer = require('ws').Server
+, http = require('http')
+, FS = require('fs')
+, bodyParser = require('body-parser')
+, express = require('express')
+, app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/app'));
+
+var server = http.createServer(app);
+server.listen(process.env.PORT);
+
+var wss = new WebSocketServer({server: server});
+
+wss.on('connection', function(ws) {
+    ws.on('message', function(data, flags) {
+        console.log(data);
+    });
+    
+    ws.on('close', function() {
+        console.log('stopping client interval');
+    });
+    
+    console.log('started client interval');
+});
